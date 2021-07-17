4b5c5fd2720fc0c71da62dd579a88cc59b00b4f3
user <farand01@naver.com>
2021-07-17 13:41:38 +0900

    initial commit

---
+++ src/test/java/com/ssm/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssm.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
